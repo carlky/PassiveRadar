dd36a3293e1c9c80a10796fb004493205d109a7e
Johan Karlsson <johan.jokar@gmail.com>
2018-04-09 10:39:50 +0200

    program-shell for plotData

---
+++ plotData.m
@@ -0,0 +1,17 @@
+% ************************************************************
+% DESCRIPTION:  A function that plots the data as a pretty graph
+%
+% INPUT:        direction:  array with all angles to target         
+%               t_delay:    array with time delay between antennas
+%               fig:        figure handle
+%
+% OUTPUT:       Pretty figures
+% WRITTEN BY:   Johan Karlsson and Oskar Lindblad 
+% STATUS:       Not finished
+% ************************************************************
+
+function plotData(direction, t_delay, fig)
+
+
+end
+
