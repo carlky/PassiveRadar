a31d22e29ba524d67ef2978a3f3a105a91948b42
Oskar Lindblad <oskar.titleist@hotmail.com>
2018-04-12 08:47:46 +0200

    Hej

--- plotData.m
+++ plotData.m
@@ -14,7 +14,7 @@
 
 function plotData(direction, t_delay, fig)
 
-        L = 1000*haversine('57 41 38N, 012 03 32E', '57 41 28N, 011 058 30E') %Antalet meter mellan oss och Brudaremossen
+        L = 1000*haversine('57 41 38N, 012 03 32E', '57 41 28N, 011 058 30E'); %Antalet meter mellan oss och Brudaremossen
         %Av någon anledning fungerar det inte att ha kommatecken i
         %funktionen haversine
         
@@ -24,19 +24,29 @@ function plotData(direction, t_delay, fig)
         
         R_rx = ((c.*t_delay(1,:)).^2 + 2*c.*t_delay(1,:)*L)./(2*c.*t_delay(1,:) + 2*L.*(1-sin(direction(1,:))));
         
-        save('R_rx.mat', 'R_rx');
-        save('direction.mat', 'direction');
+        %save('R_rx.mat', 'R_rx');
+        %save('direction.mat', 'direction');
         
 %         S = load('plot.mat', 'R_rx', 'direction'); %Ska hämta båda variablerna och lägga dem i en struct
 %         R_rx = S.R_rx;
-        R_rx = load('R_rx.mat');
-        direction = load('direction.mat');
+        %R_rx = load('R_rx.mat');
+        %direction = load('direction.mat');
 %         
 %         direction = S.direction;
 
-     
-%         polarplot(direction,R_rx,'rx')
-%         hold on
+p = polarplot(direction,R_rx,'rx');
+        p.Color = [1 0 0];
+        p.Marker = 'o';
+        p.MarkerSize = 5;
+        pax = gca; % GetCurrentAxis
+        pax.ThetaColor = [0.1 0.1 1];
+        pax.RColor = [0 1 0];
+        pax.LineWidth = 1.5;
+        pax.GridColor = [0 .7 0];
+        set(pax,'Color',[0.15 0.15 0.15]);
+        title('DETECTED TARGETS')
+        % pause(0.1)
+        hold on % to keep many     
 %         colorbar %Borde nog ligga utanför plotfunktionen så att inte den behöver uppdateras hela tiden
 %         
          clear R_rx direction
