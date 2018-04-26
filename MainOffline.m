f0849679955922e81c150d3fde6fa2d449e3c100
carlky <carlkylin@gmail.com>
2018-04-26 08:43:21 +0200

    Small change in MainOffline to improve performance

--- MainOffline.m
+++ MainOffline.m
@@ -1,9 +1,10 @@
 %% MainOffline
 clc
 % dirname=organizeFiles();
-dirName='Reading 20-Apr-2018 08-21-41';
+dirName='Reading 23-Apr-2018 10-23-42';
 centerFrequency=626e6;
 sampleRate=2e6;
+syncFrequency = 625.5e6;
 
 sampleLength=2e6;       
 %If sample frequency is 2MHz, there will be 4 Mrows/s in the data files,
@@ -14,15 +15,20 @@ l=0;
 save('readLines.mat','l');
 %The variable l contains the information about the amount of lines that has
 %been read in the data files.
-fileName=['Data/',dirName,'/capt1.cfile'];
-fileLength=length(fread(fopen(filename,'rb')));
+s = dir(['Data/',dirName,'/capt1.cfile']);
+tic
+fileLength=s.bytes;
+toc
 
-while l<=fileLength-sampleLength*2
+tic
+%while l<=fileLength-sampleLength*2
     sig = newDataOffline(centerFrequency, sampleRate, dirName, sampleLength);
+toc
+%%    
 %     sig_new = syncSignals(sig_old, syncFrequency);
 %     direction = findDirections(sig);
 %     sig_isolated = isolateDirections(sig,direction);
 %     t_delay = findDelay(sig_1,sig_2);
 %     plotData(direction, t_delay, fig);
     load readLines.mat l;
-end
+%end
