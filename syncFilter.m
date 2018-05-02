529bbc807ac0db3f09412a97d1ac7a0f8058b53e
Johan Karlsson <johan.jokar@gmail.com>
2018-05-02 14:50:28 +0200

    syncFilter changes to work with 8 dim array

--- syncFilter.m
+++ syncFilter.m
@@ -7,14 +7,15 @@
 % ---------------------------------------------
 function [Filter_signal] = syncFilter(signal)
     raw_data = signal.data;
-
-hpFilter = designfilt(  'highpassfir','StopbandFrequency',10, ...
-         'PassbandFrequency',0.5e6,'PassbandRipple',0.5, ...
+    [m,~] = size(raw_data);
+    for i=1:m
+        hpFilter = designfilt(  'highpassfir','StopbandFrequency',10, ...
+         'PassbandFrequency',0.3e6,'PassbandRipple',0.5, ...
          'StopbandAttenuation',65,'SampleRate',40e6, ...
-        'DesignMethod','kaiserwin');
-    fvtool(hpFilter)
-    Filter_signal = filtfilt(hpFilter,raw_data);
-    
+         'DesignMethod','kaiserwin');
+    % fvtool(hpFilter)  % Display filter
+    Filter_signal(i,:) = filtfilt(hpFilter,raw_data(i,:));
+    end
 end
 
 
